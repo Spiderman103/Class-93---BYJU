3e8246cc4dd6901c0b9ce5cd62592ead48ce657e
Spiderman103 <101935459+Spiderman103@users.noreply.github.com>
2022-10-30 15:46:36 -0700

    Add files via upload

---
+++ screens/ObjectType.js
@@ -0,0 +1,44 @@
+import React, {Component} from "react";
+import {View, Text,StyleSheet, TouchableOpacity} from "react-native";
+
+export default class ObjectTypeScreen extends Component{
+    
+    render(){
+        return(
+            <View style={styles.container}>
+        <Text style={{ fontSize:30, backgroundColor:"yellow"}}> ObjectType </Text>
+        
+        <Text> </Text>
+        <Text style={styles.points}>1) Text1..........</Text>
+        <Text> </Text>
+        <Text style={styles.points} >2) Text2 {"\n"} text3.</Text>
+
+        <TouchableOpacity
+              style={styles.buttons}
+              onPress={() => this.props.navigation.navigate('FeedScreen')}>
+              <Text style={{ fontSize:20, color:"white"}}>Back</Text>
+        </TouchableOpacity>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container:{ 
+       alignItems: 'center',
+       justifyContent: 'center',
+       marginTop: 100 
+      },
+    points:{
+      fontSize: 20
+    },  buttons: {
+      alignItems: 'center',
+      justifyContent: 'center',
+      borderWidth: 2,
+      borderRadius: 15,
+      backgroundColor:"red",
+      margin: 10,
+      width: 200,
+      height: 50,
+    }
+  })
